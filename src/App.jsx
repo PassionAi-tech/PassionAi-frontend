112009704dc6115688f3233d7eab0423a38b78c5
PassionAi-tech <passionai2010@gmail.com>
2026-07-15 15:42:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ import { ResultsScreen } from "./components/ResultsScreen";
 import { QuestsScreen } from "./components/QuestsScreen";
 import { ProfileScreen } from "./components/ProfileScreen";
 import { BottomNav } from "./components/BottomNav";
-import { UNIT_STAGES, UNIT_XP } from "./data/curriculum";
+import { UNIT_STAGES, UNIT_XP } from "./data/Curriculum";
 import { fetchIntro, fetchDeepDive, fetchMastery, FB_INTRO, FB_DEEPDIVE, FB_MASTERY } from "./services/api";
-import { C } from "./styles/theme";
+import { C } from "./styles/Theme";
 
 export default function PassionAI() {
   const [screen,     setScreen]     = useState("register");
